786bad0de9c4b34a45b246d22cf6c8d24a1ede8d
ashrafemad <ashrafemad23@gmail.com>
2018-04-19 19:07:13 +0200

    test

--- news_root/wsgi.py
+++ news_root/wsgi.py
@@ -10,10 +10,11 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "news_root.settings")
 
 application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
