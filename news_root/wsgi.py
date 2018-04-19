824d7bb103f3a99eba0601944b85225eacab6486
ashrafemad <ashrafemad23@gmail.com>
2018-04-19 19:00:59 +0200

    test

--- news_root/wsgi.py
+++ news_root/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "news_root.settings")
 
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
